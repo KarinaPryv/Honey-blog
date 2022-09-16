ae65324fb92e9f31719ecb1354774c9ea0cae502
KarinaPryv <94608784+KarinaPryv@users.noreply.github.com>
2022-09-16 12:46:50 +0300

    Update apiMethods.js

--- src/common/_services/apiMethods.js
+++ src/common/_services/apiMethods.js
@@ -4,27 +4,28 @@ const baseUrl = 'https://www.googleapis.com/blogger/v3';
 const SELF_BLOGS = "/users/self/blogs";
 const BLOGS_ENDPOINT = "/blogs";
 const POSTS_ENDPOINT = "/posts";
+const API_KEY = "AIzaSyCmL01pEYb0LP5NShDvoXVGJzutWlVwKPI"
 
 export const getBlogsByUser = () => {
     return axios.get(`${baseUrl}${SELF_BLOGS}?key=${process.env.REACT_APP_API_KEY}`);
 }
 
 export const getPostsByBlogId = (blogId) => {
-    return axios.get(`${baseUrl}${BLOGS_ENDPOINT}/${blogId}${POSTS_ENDPOINT}?key=${process.env.REACT_APP_API_KEY}&maxResults=100`);
+    return axios.get(`${baseUrl}${BLOGS_ENDPOINT}/${blogId}${POSTS_ENDPOINT}?key=${API_KEY}&maxResults=100`);
 }
 
 export const getPostById = (blogId, postId) => {
-    return axios.get(`${baseUrl}${BLOGS_ENDPOINT}/${blogId}${POSTS_ENDPOINT}/${postId}?key=${process.env.REACT_APP_API_KEY}`);
+    return axios.get(`${baseUrl}${BLOGS_ENDPOINT}/${blogId}${POSTS_ENDPOINT}/${postId}?key=${API_KEY}`);
 }
 
 export const deletePostById = (blogId, postId) => {
-    return axios.delete(`${baseUrl}${BLOGS_ENDPOINT}/${blogId}${POSTS_ENDPOINT}/${postId}?key=${process.env.REACT_APP_API_KEY}`);
+    return axios.delete(`${baseUrl}${BLOGS_ENDPOINT}/${blogId}${POSTS_ENDPOINT}/${postId}?key=${API_KEY}`);
 }
 
 export const addNewPost = (blogId, postModel) => {
-    return axios.post(`${baseUrl}${BLOGS_ENDPOINT}/${blogId}${POSTS_ENDPOINT}?key=${process.env.REACT_APP_API_KEY}`, postModel)
+    return axios.post(`${baseUrl}${BLOGS_ENDPOINT}/${blogId}${POSTS_ENDPOINT}?key=${API_KEY}`, postModel)
 }
 
 export const updatePostById = (blogId, postId, postModel) => {
-    return axios.put(`${baseUrl}${BLOGS_ENDPOINT}/${blogId}${POSTS_ENDPOINT}/${postId}?key=${process.env.REACT_APP_API_KEY}`, postModel);
+    return axios.put(`${baseUrl}${BLOGS_ENDPOINT}/${blogId}${POSTS_ENDPOINT}/${postId}?key=${API_KEY}`, postModel);
 }
